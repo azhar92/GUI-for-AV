f6b9f95db5e86ab02589111b1854260925fa2517
azhar92 <a0110710@u.nus.edu>
2016-12-13 14:07:33 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label_2->setText("Autonomous");
+
+}
+
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->label_2->setText("Supervisory");
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->label_2->setText("Manual");
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    ui->label_2->setText("Parked");
+}
